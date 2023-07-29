c0031e3e595ae6bd587a38d3eaac2c94f1e5e8ab
mohcine-boudenjal <mohcine.boudenjal@etu.uae.ac.ma>
2023-07-29 16:28:19 +0100

    add two contracts example : cdi and cdd format html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CddComponent } from './components/cdd/cdd.component';
+import { CdiComponent } from './components/cdi/cdi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'cdd', component: CddComponent },
+  { path: 'cdi', component: CdiComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
